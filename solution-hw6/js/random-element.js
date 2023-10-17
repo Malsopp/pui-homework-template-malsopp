c48d347150491923cf2fba319ae089bca4cf39ab
Malsopp <143577279+Malsopp@users.noreply.github.com>
2023-10-17 02:44:51 -0400

    HW 6 Changes

---
+++ solution-hw6/js/random-element.js
@@ -0,0 +1,131 @@
+
+
+// dictionary for glazing information
+let glazing = {
+    "Keep original": 0,
+    "Sugar milk": 0,
+    "Vanilla milk": 0.5,
+    "Double chocolate": 1.50,
+};
+
+// dictionary for packing information
+let packing = {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+};
+
+// selecting glazing drop down menu
+let selectglazing = document.querySelector("#Glazing");
+console.log(selectglazing);
+
+// looping to add glazing options
+for (const key in glazing) {
+    var optionglazing = document.createElement("option");
+    optionglazing.text = key;
+    optionglazing.value = glazing[key];
+    selectglazing.appendChild (optionglazing);
+}
+
+// selecting pack size from the drop-down menu
+let selectpacksize = document.querySelector("#packSize");
+
+
+// looping to add packing options
+for (const key in packing) {
+    var optionpacking = document.createElement("option");
+    optionpacking.text = key;
+    optionpacking.value = packing[key];
+    selectpacksize.appendChild(optionpacking);
+}
+
+// base price for the items when they are unedited and allow to compute new values
+let glazingPrice = 0.0;
+let packPrice = 1;
+let basePrice = 2.49;
+
+// create Event Listeners whenever the drop dopwn menus are selected
+selectglazing.addEventListener ("change",updateGlazing);
+selectpacksize.addEventListener ("change",updatePackSize);
+
+// select the individual price 
+let currentPrice = document.getElementsByClassName ("indvPrice");
+
+// update the total price of the items with the pack size and glazing updated
+function updateDisplay(rollPrice) {
+    let totalPrice = ((basePrice + glazingPrice) * packPrice);
+    console.log(totalPrice);
+    let roundedTotalPrice = totalPrice.toFixed(2);
+    for (let i = 0; i < currentPrice.length; i++) {
+        currentPrice[i].textContent = "$" + roundedTotalPrice;
+    }
+}
+
+// update glazing price variable
+function updateGlazing() {
+    glazingPrice = parseFloat(selectglazing.value);
+    console.log(glazingPrice);
+    updateDisplay();
+}
+
+// update pack size price
+function updatePackSize() {
+    packPrice = parseFloat(selectpacksize.value);
+    console.log(packPrice);
+    updateDisplay();
+}
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get("roll");
+
+
+//how to identify each individual item's name, base price, and image
+let header1 = document.querySelector("h1");
+let rollName = rolls[rollType].name;
+let rollPrice = rolls[rollType].basePrice;
+let rollImg = rolls[rollType].imageFile;
+header1.textContent = rollName;
+
+
+//how to identify each individual's price and image 
+document.querySelector(".indvPrice").textContent = "$" + rollPrice;
+document.querySelector("#detailImg").src = "../assets/products/" + rollImg;
+
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+// add items with customizations to console
+
+function addToCartClicked() {
+    let cart = [];
+
+     // check if local storage contains something named cart
+     if (localStorage.getItem("cart") != null) {
+        const cartString = localStorage.getItem("cart");
+        const cartArray = JSON.parse(cartString);
+        for (const cartData of cartArray) {
+          const rollInstance = new Roll (cartData.type, cartData.glazing, cartData.size, cartData.basePrice);
+          cart.push (rollInstance);
+        }
+      }
+
+    var rollGlazing = selectglazing.options[selectglazing.selectedIndex].text;
+    var packSize = selectpacksize.options[selectpacksize.selectedIndex].text;
+    const rollInstance = new Roll(rollType, rollGlazing, packSize, rollPrice);
+    cart.push(rollInstance);
+
+    const cartArrayString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartArrayString);
+    console.log(cart); 
+}
+
+
---
+++ solution-hw6/js/update-shopping-cart.js
@@ -0,0 +1,103 @@
+// dictionary for glazing information
+let glazing = {
+    "Original": 0,
+    "Sugar milk": 0,
+    "Vanilla milk": 0.5,
+    "Double chocolate": 1.50,
+};
+
+// dictionary for packing information
+let packing = {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+};
+
+//return the individual item price for each item in the cart
+function itemPrice(basePrice,rollGlazing,packSize) {
+    let itemPrice = ((basePrice + glazing[rollGlazing]) * packing[packSize]);
+    return itemPrice;
+}
+
+//given class with role
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+        this.itemPrice = itemPrice(basePrice, rollGlazing, packSize);
+
+    }
+}
+
+//Check if there is something in local storage (ie. a cart)
+let cart = [];
+
+// check if local storage contains something named cart
+if (localStorage.getItem("cart") != null) {
+   const cartString = localStorage.getItem("cart");
+   const cartArray = JSON.parse(cartString);
+   for (const cartData of cartArray) {
+     const rollInstance = new Roll (cartData.type, cartData.glazing, cartData.size, cartData.basePrice);
+     cart.push (rollInstance);
+   }
+ }
+
+function addToCartDOM(rollInstance) {
+    //select the item cart class
+    const template = document.querySelector('.item_cart');
+    const clone = template.content.cloneNode(true);
+
+    //This line is finding an element with the class "item" inside the cloned content and assigning it to a property element of an object called rollInstance
+    rollInstance.element = clone.querySelector('.item');
+
+    //This line is finding an img element inside the rollInstance.element which is a reference to an image associated with a product.
+    let cartImg = rollInstance.element.querySelector('img');
+    cartImg.src = "../assets/products/" + rolls[rollInstance.type].imageFile;
+
+    let cartText = rollInstance.element.querySelector ('.cart_description_text');
+    //This is setting the inner HTML of the element found in the previous line. It's creating a description for a product based on properties of the rolls array and rollInstance object.
+    cartText.innerHTML = rolls[rollInstance.type].name + "<br>" +
+                      "Glazing: " + rollInstance.glazing + "<br>" +
+                      "Pack size: " + rollInstance.size;
+    
+    let cartPrice = rollInstance.element.querySelector ('.cart_price_text');
+    cartPrice.innerHTML = "$" + rollInstance.itemPrice.toFixed(2);
+
+    let cartList = document.querySelector('#cart_list');
+    cartList.appendChild(rollInstance.element);
+    
+    //remove element inside rollInstnace
+    rollInstance.element.querySelector(".remove").addEventListener("click", () => {removeCartItem(rollInstance)});
+}    
+
+function removeCartItem (rollInstance){
+    rollInstance.element.remove();
+    // this code removes the roll & all of its attributes from the array
+    cart.pop(rollInstance);
+    totalPriceDisplay.textContent = "$" + updateTotalPrice().toFixed(2);
+
+    const cartArrayString = JSON.stringify(cart);
+    localStorage.setItem("cart", cartArrayString);
+    console.log(cart); 
+}
+
+// this updates the total price of the product
+function updateTotalPrice (){
+    let totalPrice = 0;
+    for (const roll of cart){
+        totalPrice += roll.itemPrice;
+    }
+    return totalPrice;
+}
+
+//updates the display of the total price with two variables
+totalPriceDisplay = document.querySelector(".numerical");
+totalPriceDisplay.textContent = "$" + updateTotalPrice().toFixed(2);
+
+// Add the Rolls to the cart DOM
+for (const roll of cart) {
+    addToCartDOM(roll);
+}
